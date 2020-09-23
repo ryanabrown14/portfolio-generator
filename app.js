1a9ce1093b2700b371fdcc39f749431f9f3a59f9
ry brown <ryan.alex.brown@gmail.com>
2020-09-23 15:24:43 -0700

    changes

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 
 const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }
+    profileDataArr.forEach(profileItem =>console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
