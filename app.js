b4543cb2a7270b4898949de8932354ff0c6a7d68
ry brown <ryan.alex.brown@gmail.com>
2020-09-24 20:01:32 -0700

    when statement

--- app.js
+++ app.js
@@ -30,9 +30,23 @@ return inquirer.prompt([
     }
 },
 {
+    type: 'confirm',
+    name: 'confirmAbout',
+    message: 'Would you like to enter some information about yourself for an "About" section?',
+    default: true
+},
+{
     type:'input',
     name: 'about',
-    message: 'provide some information about yourself:'
+    message: 'provide some information about yourself:',
+    when: ({confirmAbout}) => {
+        if (confirmAbout) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
 
 
