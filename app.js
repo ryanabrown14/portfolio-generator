abb35d089f5107a2c2666686f79c85070144a57a
ry brown <ryan.alex.brown@gmail.com>
2020-09-25 17:57:11 -0700

    HTML done

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const promptUser = () => {
 return inquirer.prompt([
 {
@@ -139,17 +141,16 @@ Add a New Project
 };
 
 promptUser()
-.then(promptProject)
-.then(portfolioData => {
-  console.log(portfolioData);
-});
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
 
-//const pageHTML = generatePage(name, github);
+     fs.writeFile('./index.html', pageHTML, err => {
+         if (err) throw new Error(err);
 
-//fs.writeFile('./index.html', pageHTML, err => {
-//  if (err) throw err;
+       console.log('Page created! Check out index.html in this directory to see it!');
+     });
+  });
 
-//  console.log('Portfolio complete! Check out index.html to see the output!');
-//});
+
+
